75ed8551d07e198bb574034ca85de9c8d7575565
fitsum maru <Fitsum Maru>
2022-12-03 15:04:32 -0500

    Fitsum's commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'cs336-finalproject-1fd79',
+    appId: '1:943195271889:web:ca9266957800b526038bf0',
+    storageBucket: 'cs336-finalproject-1fd79.appspot.com',
+    apiKey: 'AIzaSyALf42FcdUiLWHJ1c_eKGneoRH4fJPAuaw',
+    authDomain: 'cs336-finalproject-1fd79.firebaseapp.com',
+    messagingSenderId: '943195271889',
+    measurementId: 'G-QWN3LVTWC5',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'cs336-finalproject-1fd79',
+    appId: '1:943195271889:web:ca9266957800b526038bf0',
+    storageBucket: 'cs336-finalproject-1fd79.appspot.com',
+    apiKey: 'AIzaSyALf42FcdUiLWHJ1c_eKGneoRH4fJPAuaw',
+    authDomain: 'cs336-finalproject-1fd79.firebaseapp.com',
+    messagingSenderId: '943195271889',
+    measurementId: 'G-QWN3LVTWC5',
+  },
   production: false
 };
 
