2e9064d108132425ca1390567deb13f27ddd25b0
samuelth47 <samtad4774@gmail.com>
2022-12-04 22:23:11 -0500

    Clean up and Updated files for the corresponding name and search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,42 +3,40 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import * as firebase from 'firebase/compat/app';
 import { Observable } from 'rxjs';
 
+export interface FirestoreRec {
+  name: string
+  searchQuery: string
+  timestamp: firebase.default.firestore.Timestamp
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'hmwk10-chatapp';
-  message$ : FirestoreRec[] = [];
+  title = 'countryTrivia';
 
-  name : string = localStorage.getItem("name")!; 
-  color : string  = localStorage.getItem("color")!;
-  message : string = "";
+  name: string = localStorage.getItem("name")!;
+  searchQuery: string = "";
+  searchArray: FirestoreRec[] = [];
 
   constructor(private firestore: AngularFirestore) {
-    this.getMessages().subscribe(result => this.message$ = result);
+    this.getMessages().subscribe(result => this.searchArray = result);
   }
 
-  getMessages() : Observable<FirestoreRec[]>{
-    return this.firestore.collection<FirestoreRec>('messages', ref => ref.orderBy('timestamp')).valueChanges()
+  getMessages(): Observable<FirestoreRec[]> {
+    return this.firestore.collection<FirestoreRec>('searchQueries', ref => ref.orderBy('timestamp')).valueChanges()
   }
 
-  sendMessage() : void {
-    this.firestore.collection<FirestoreRec>('messages').add({
-      message: this.name + ': ' + this.message,
+  sendQuery(): void {
+    this.firestore.collection<FirestoreRec>('searchQueries').add({
+      name: this.name,
+      searchQuery: this.name + ': ' + this.searchQuery,
       timestamp: new firebase.default.firestore.Timestamp(Date.now() / 1000, 0),
-      color: this.color,
     });
-    this.message = "";
+    this.searchQuery = "";
   }
 
   saveName() { localStorage.setItem("name", this.name); }
-  saveColor() { localStorage.setItem("color", this.color); }
-}
-
-export interface FirestoreRec {
-  message: string
-  timestamp: firebase.default.firestore.Timestamp
-  color ? : string
 }
