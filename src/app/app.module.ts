f93afcea40494aa475362c3ffb88f0859d575493
samuelth47 <samtad4774@gmail.com>
2022-12-05 01:32:06 -0500

    Integrated the API with the firestore database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { environment } from '../environments/environment';
 import { RecentSearchesComponent } from './components/recent-searches/recent-searches.component';
+import { ResultComponent } from './components/result/result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecentSearchesComponent
+    RecentSearchesComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
